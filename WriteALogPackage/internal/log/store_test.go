c45c57d86b76a25b88f8ff8092d70b6efa4c25d4
ppmasa8 <kiyokiyopip56@gmail.com>
2023-01-07 01:56:20 +0900

    on the way

---
+++ WriteALogPackage/internal/log/store_test.go
@@ -0,0 +1,26 @@
+package log
+
+import "os"
+
+var (
+	write = []byte("hello world!")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
