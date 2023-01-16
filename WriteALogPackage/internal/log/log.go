2b8d9fbb16c642c6b40c13cbe275eda6dd2fdc52
ppmasa8 <kiyokiyopip56@gmail.com>
2023-01-16 21:48:50 +0900

    Added

---
+++ WriteALogPackage/internal/log/log.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	api "github.com/ppmasa8/proglog/api/v1"
+)
+
+type Log struct {
+	mu sync.RWMutex
+
+	Dir    string
+	Config Config
+
+	activeSegment *segment
+	segments      []*segment
+}
