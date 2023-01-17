b8d3d6d8a632e926f1159252c923bb823b0cc28d
ppmasa8 <kiyokiyopip56@gmail.com>
2023-01-17 23:41:16 +0900

    added

--- WriteALogPackage/internal/log/log.go
+++ WriteALogPackage/internal/log/log.go
@@ -22,3 +22,50 @@ type Log struct {
 	activeSegment *segment
 	segments      []*segment
 }
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir: dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		i++
+	}
+	if l.segments == nil {
+		if err = l.newSegment(
+			l.Config.Segment.InitialOffset,
+		); err != nil {
+			return err
+		}
+	}
+	return nil
+}
