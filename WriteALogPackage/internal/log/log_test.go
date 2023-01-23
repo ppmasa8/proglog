be94779aa0b99c8dbca88839eea545e8b2f93a83
ppmasa8 <kiyokiyopip56@gmail.com>
2023-01-23 20:15:55 +0900

    added

---
+++ WriteALogPackage/internal/log/log_test.go
@@ -0,0 +1,36 @@
+package log
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	api "github.com/travisjeffery/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error": 		 testOutOfRangeErr,
+		"init with existing segments":		 testInitExisting,
+		"reader":							 testReader,
+		"truncate":							 testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
