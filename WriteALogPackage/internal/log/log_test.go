e421aa6331ab6ce523f60a60ffd7a450d5b1c899
ppmasa8 <kiyokiyopip56@gmail.com>
2023-01-27 22:34:44 +0900

    added

--- WriteALogPackage/internal/log/log_test.go
+++ WriteALogPackage/internal/log/log_test.go
@@ -55,3 +55,32 @@ func testOutOfRangeErr(t *testing.T, log *Log) {
 	require.Error(t, err)
 	require.NoError(t, log.Close())
 }
+
+func testInitExisting(t *testing.T, o *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := o.Append(append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, o.Close())
+
+	off, err := o.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = o.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	n, err := NewLog(o.Dir, o.Config)
+	require.NoError(t, err)
+
+	off, err = n.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = n.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+	require.NoError(t, n.Close())
+}
