02208946f5e9abfce8dfc80d5e45dfa0458108d6
ppmasa8 <kiyokiyopip56@gmail.com>
2023-02-21 22:39:41 +0900

    added test

---
+++ ServeRequestsWithgRPC/internal/server/server_test.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"context"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	api "github.com/ppmasa8/proglog/api/v1"
+	"github.com/ppmasa8/proglog/internal/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce.consume stream  succeeds": testProduceConsumeStream,
+		"consume past log boundary fails": testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
